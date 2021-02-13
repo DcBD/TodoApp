7697b14cbe8cb39905efcfa7e6e2c0b05f9490d5
David <dawid.prosba@gmail.com>
2021-02-13 20:01:37 +0100

    Creating task view design

--- DesktopWPFUI/Controls/NavigationBar.xaml.cs
+++ DesktopWPFUI/Controls/NavigationBar.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DesktopWPFUI.Windows;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,10 +21,21 @@ namespace DesktopWPFUI.Controls
     /// </summary>
     public partial class NavigationBar : UserControl
     {
+        private Window CreateTaskWindow { get; set; }
         public NavigationBar()
         {
             InitializeComponent();
         }
 
+        private void CreateTaskButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (CreateTaskWindow != null) CreateTaskWindow.Close();
+
+            CreateTaskWindow = new CreateTaskWindow();
+
+            CreateTaskWindow.Show();
+
+        }
     }
 }
---
+++ DesktopWPFUI/Windows/CreateTaskWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TodoApp.Domain.Services;
+using TodoApp.EntityFramework.Services;
+
+namespace DesktopWPFUI.Windows
+{
+    /// <summary>
+    /// Logika interakcji dla klasy CreateTaskWindow.xaml
+    /// </summary>
+    public partial class CreateTaskWindow : Window
+    {
+        public CreateTaskWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSaveHandleClick(object sender, RoutedEventArgs e)
+        {
+            IDataService<TodoApp.Domain.Models.Task> taskService = new GenericDataService<TodoApp.Domain.Models.Task>(new TodoApp.EntityFramework.TodoAppDbContextFactory());
+            
+
+            taskService.Create(new TodoApp.Domain.Models.Task 
+                { 
+                    Name = this.Name.Text,
+                    Description = this.Description.Text,
+                    IsFinished = false,
+                    Start = new DateTime(),
+                });
+
+            Console.WriteLine("Saved");
+        }
+    }
+}
